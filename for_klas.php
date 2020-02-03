5a95365638632af34530adef3289107ef7ebe527
HENRY BENAYE <2106747@talnet.nl>
2020-02-03 14:26:31 +0100

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,9 @@
+<?php
+$namen = ['Sjonnie de Wiel','Herman Kaal','Henk de Steen','Inge Kerkhoven','Gert Kruiswijk'];
+$size =count($namen);
+
+for($i = 0; $i <= $size; $i++){
+
+    echo $namen[$i] .PHP_EOL;
+}
+?>
